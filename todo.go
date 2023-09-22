8acc7a9931b4e4a8c0c59150c123c9ff39c4dbd2
VictorDMoura <mouramvictor@gmail.com>
2023-09-22 15:16:56 -0300

    feat: Add add method

--- todo.go
+++ todo.go
@@ -8,3 +8,17 @@ type item struct {
 	CreatedAt   time.Time
 	CompletedAT time.Time
 }
+
+type List []item
+
+// Add creates  a new todo item and appends it to the list
+func (l *List) Add(task string) {
+	t := item{
+		Task:        task,
+		Done:        false,
+		CreatedAt:   time.Now(),
+		CompletedAT: time.Time{},
+	}
+
+	*l = append(*l, t)
+}
